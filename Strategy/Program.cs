fc889b25d57043bc35007503c0bc3a9a3ff33cf3
Anton Kamolov <kamolov.2004@mail.ru>
2023-12-09 13:26:23 +0300

    Первый коммит

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -14,10 +14,9 @@ namespace Strategy
             Player player = new Player("Главный герой", 20);
             Player anton = new Player("Антон", 50);
             Enemy enemy = new Enemy("Слабак", 5, 15);
-            Enemy lox = new Enemy("Лох", 10, 20);
             anton.SetWeapon(new Sword());
             Events.StartGame(anton);
-            Events.EnemyDuel(anton, lox);
+            Events.EnemyDuel(anton, enemy);
             Console.ReadLine();
         }
     }
